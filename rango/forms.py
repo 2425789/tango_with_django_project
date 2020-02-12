cad3a5d54a230673c1fe38bc86c2f2bdb2cc9ce5
Antonius Fionaldi <2425789f@student.gla.ac.uk>
2020-02-12 07:23:02 +0000

    Chapter 7, test error saying /rango/category/python/add_page/ is not there, even though it is

--- rango/forms.py
+++ rango/forms.py
@@ -2,20 +2,28 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.Charfield(max_length=128,help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    slug = forms.Charfield(widget=forms.HiddenInput(), required=False)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
     
     class Meta:
         model = Category
         fields = ('name',)
         
-Class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,help_text="Please enter the title of the page.")
-    url = forms.URLFIELD(max_length=200,help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(widget-forms.HiddenInput(), initial=0)
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200,help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
     
     class Meta:
-    model = Page
-    exclude = ('category',)
+        model = Page
+        exclude = ('category',)
